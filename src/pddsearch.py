994e44eb49b53149a9a6c7de94a866618c5c86ef
John.Li <lijianxiong5201994@163.com>
2022-01-10 21:58:16 +0800

    add login page

--- src/pddsearch.py
+++ src/pddsearch.py
@@ -6,69 +6,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import csv
+import itertools
 
-def getHtmlText(url):
-    try:
-        header = {
-            'cache-control': 'max-age=0',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41',
-            'sec-fetch-user': '?1',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-dest': 'document',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            "Referer": "http://mobile.pinduoduo.com/",
-            'cookie': '',
-        }
-        r = requests.get(url, headers=header)
-        r.raise_for_status()
-        r.encoding = r.apparent_encoding
+def login_page(driver):
+    user_name = '18682468446'
+    #footer-item-icon-wrap
+    footer_items = driver.find_elements_by_xpath('//*[@class="footer-item-icon-wrap"]')
+    footer_items[len(footer_items) - 1].click()
+    driver.find_element_by_xpath('//*[@class="phone-login"]').click()
 
-
-
-        return r.text
-    except:
-        print("爬取失败")
-        return ""
-
-
-def parsePage(ilist, html):
-    try:
-        goods_info = re.findall('VGc5+Y0S a9bD-5Ut *', html)
-        plt = re.findall(r'"view_price":"\d+.\d*"', html)
-        tlt = re.findall(r'"raw_title":".*?"', html)
-        slt = re.findall(r'"nick":".*?"', html)
-        addlt = re.findall(r'"item_loc":".*?"', html)
-        qlt = re.findall(r'"view_sales":".*?"', html)
-        # print(tlt)
-        print(len(plt))
-        for i in range(len(plt)):
-            price = eval(plt[i].split(':')[1])
-            title = eval(tlt[i].split(':')[1])
-            shop = eval(slt[i].split(':')[1])
-            address = eval(addlt[i].split(':')[1])
-            quality = eval(qlt[i].split(':')[1])
-            ilist.append([title, price, shop, address, quality])
-
-        # print(ilist)
-    except:
-        print("解析出错")
-
-
-def printGoodsList(ilist, num):
-    table = PrettyTable(["序号", "商品名称", "价格", "商家名称", "商家地址", "销量"])# add more info here
-    count = 0
-    for g in ilist:
-        count += 1
-        if count <= num:
-            table.add_row([count,g[0],g[1],g[2],g[3],g[4]])
-    print(table)
-
+    driver.find_element_by_id('user-mobile').send_keys(user_name)
+    driver.find_element_by_id('code-button').click()
+    pincode_input = driver.find_element_by_id('input-code')
+    
 
 def search_product(driver, keywords):
+    username = "18682468446"
+
     driver.maximize_window()
     # click text box 2022.01.05
     driver.find_element_by_class_name('_18v23kPu').click()
@@ -78,19 +32,39 @@ def search_product(driver, keywords):
     search_text = driver.find_element_by_class_name('QahmZDd2')
     search_text.send_keys(keywords)
     search_text.send_keys(Keys.ENTER)
-    count = 0
+
     time.sleep(0.5)
     items_titles = driver.find_elements_by_class_name('PWKq3gf1')
 
     # find item info via find elements by class name
-    items_names = driver.find_elements_by_class_name('fnpJrQyt KbqLm0ek')
+    # items_names = driver.find_elements_by_class_name('fnpJrQyt KbqLm0ek')
     # items_price = driver.find_elements_by_class_name('_9D91bFn1')
     # items_salecount = driver.find_elements_by_class_name('jmOJMlWq')
 
     # find items info via xpath
     # page = driver.find_elements_by_xpath('//*[@id="main"]')[0].text
     # print(page)
-    page = driver.find_elements_by_xpath('//*[@class="RIo5XeMZ"]')[0].text
+
+    # get all items only 22, one page 
+    items = driver.find_elements_by_xpath('//*[@class="RIo5XeMZ"]')
+    for item in items:
+        item.click()
+        time.sleep(0.5)
+        # find the "发起拼单" Qzax7E1w
+        pindan = driver.find_element_by_xpath('//*[@class="Qzax7E1w"]')
+        pindan.click()
+        time.sleep(0.5)
+        # various classification: r-mksVqr
+        skus = driver.find_elements_by_xpath('//*[@class="sku-specs-key"]') #sku-specs-key sku for goods
+        for sku in skus:
+        # color classification: qK4302ba
+        # selected specific type:tWGpNA2Y
+            ssr = sku.find_elements_by_xpath('.//div/*')
+            
+            # price of specific product: _27FaiT3N
+            price = driver.find_elements_by_xpath('//*[@class="_27FaiT3N"]')
+
+        
     itemsname_list = []
     time.sleep(0.5)
     # fnpJrQyt
@@ -99,15 +73,17 @@ def search_product(driver, keywords):
         print(item.text)
         itemsname_list.append(item.text)
 
-
-    
-    search_html = driver.find_element_by_tag_name('html')#获取对应标签
-    height=search_html.size['height'] * 21#获取html页面的总高度
- 
+    # region  lazy load
+    # get labels
+    search_html = driver.find_element_by_tag_name('html')
+    # get html page height
+    height=search_html.size['height'] * 21 
     for i in range(700,height,700):
         s=f'window.scrollBy(0,700)'#每次划700的单位
         driver.execute_script(s)   #向下滚动，0在第一位是向上向下，0在第二位是向左向右，负号决定具体方向
         time.sleep(1.5)
+    #endregion
+
 
     items_specs = driver.find_elements_by_xpath('//*[@class="NA5750pm"]')
     itemsspecs_list = []
@@ -125,19 +101,24 @@ def search_product(driver, keywords):
 # def pddlogin(username, password):
 
 
-def pddSearch(name):
+def pddSearch(name, brand="", serial_number="", size="", color=""):
     infoList = []
     # initialize driver
+    option=webdriver.ChromeOptions()
+    user_data_dir=r'C:\Users\Home_JLI\AppData\Local\Google\Chrome\User Data'
+    option.add_argument(f'--user-data-dir={user_data_dir}')
     while 1:
         try:
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(options=option)
             break
         except Exception:
             time.sleep(1)
-
+    # get all info can be type in searchbox
+    keyword = name + brand + serial_number
     # request page
     driver.get("https://mobile.pinduoduo.com/")
-    search_product(driver, name)
+    login_page(driver)
+    search_product(driver, keyword)
     # 通过page_source获取网页源代码
     print(driver.page_source)
 
@@ -150,4 +131,4 @@ def pddSearch(name):
             w.writerow(v)
     print("结束保存")
 
-pddSearch("充电宝")
+pddSearch("充电宝", "华为", "60W", "快充")
